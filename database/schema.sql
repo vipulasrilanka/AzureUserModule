dacc2a291aeb3a39ca51e29ee7c8674f2dd5e6c1
Vipula Liyanaarachchi <vipulasrilanka@yahoo.com>
2025-03-23 14:50:52 +0530

    Updated database schema and test data with comprehensive IoT device management structure

--- database/schema.sql
+++ database/schema.sql
@@ -1,3 +1,8 @@
+-- This file has all the details about the database
+-- There are multiple tables, and views, and below are the explainaitons.
+
+
+-- [dbo].[Users], Table. This as the data about users. This does not have external links.
 CREATE TABLE [dbo].[Users] (
     UserID INT NOT NULL IDENTITY(100000,1) PRIMARY KEY,
     UserName VARCHAR(20) NOT NULL,
@@ -9,18 +14,33 @@ CREATE TABLE [dbo].[Users] (
     CreatedDateTime DATETIME NOT NULL DEFAULT GETDATE(),
 );
 
+-- [db0].[UserPasswords], Table, This holds the password hash and other data related to login.
+CREATE TABLE [dbo].[UserPasswords] (
+    UserID INT NOT NULL PRIMARY KEY,
+    PasswordHash NVARCHAR(64) NOT NULL,  -- SHA-256 hash is 64 characters
+    ResetFunctionInput NVARCHAR(MAX) NOT NULL, -- excrypted input to the reset option
+    LastUpdated DATETIME NOT NULL DEFAULT GETDATE(),
+    FOREIGN KEY (UserID) REFERENCES [dbo].[Users](UserID)
+);
+
+-- [dbo].[DeviceTypes], Device types like Door lock, Gas Valve,..etc
 CREATE TABLE [dbo].[DeviceTypes] (
     DeviceTypeID INT NOT NULL IDENTITY(100000, 1) PRIMARY KEY,
     TypeName NVARCHAR(50) NOT NULL UNIQUE,
     TypeDescription NVARCHAR(MAX) NOT NULL,
 );
 
+-- [dbo].[EventTypes], State Change (comes from device status), Activate (true/false), 
+-- Register (true/false), Error, Note (which can hold a custom text)
 CREATE TABLE [dbo].[EventTypes] (
     EventTypeID INT NOT NULL IDENTITY(100000, 1) PRIMARY KEY,
     EventTypeName NVARCHAR(50) NOT NULL UNIQUE,
     EventTypeDescription NVARCHAR(MAX) NOT NULL,
 );
 
+-- [dbo].[EventValues] for each event type, there are multiple valid values. 
+-- For State change, they come from Device status, but for Register..etc it can be limited to 
+-- true/false. This table has the acceptable value list for all Event Types.
 CREATE TABLE [dbo].[EventValues] (
     EventValueID INT NOT NULL IDENTITY(100000, 1) PRIMARY KEY,
     EventValue NVARCHAR(20) NOT NULL,
@@ -29,16 +49,33 @@ CREATE TABLE [dbo].[EventValues] (
     FOREIGN KEY (EventTypeID) REFERENCES [dbo].[EventTypes](EventTypeID)
 );
 
-CREATE TABLE [dbo].[StateTypes] (
-    StateTypeID INT NOT NULL IDENTITY(100000, 1) PRIMARY KEY,
-    FriendlyName NVARCHAR(20) NOT NULL,
-    NameText NVARCHAR(50) NOT NULL,
-    StateSetTo NVARCHAR(50) NOT NULL,
-    DeviceTypeID INT NOT NULL,
-    FunctionID VARCHAR(20) NOT NULL,
-    FOREIGN KEY (DeviceTypeID) REFERENCES [dbo].[DeviceTypes](DeviceTypeID)
+-- [dbo].[Functions] List of functions that can be called by devices, when a state change 
+-- happens. 
+CREATE TABLE [dbo].[Functions] (
+    FunctionID INT NOT NULL IDENTITY(100000,1) PRIMARY KEY,
+    FunctionName VARCHAR(20) NOT NULL UNIQUE,
+    FunctionDescription NVARCHAR(MAX) NOT NULL, -- what does this do
+    InputCount INT NOT NULL, -- number of inputs it can take.
+    CreatedDateTime DATETIME NOT NULL DEFAULT GETDATE()
 );
 
+-- [dbo].[StateChangeTypes] applicable State types for each device type. This will be 
+-- the same that we record in events under normal STATE_CHANGE. For each device 
+-- there will be a list of states, for example, a type DOOR_LOCK can have LOCK, 
+-- UNLOCK, ENABLE and DISABLE. 
+CREATE TABLE [dbo].[StateChangeTypes] (
+    StateTypeID INT NOT NULL IDENTITY(100000, 1) PRIMARY KEY,
+    StateChangeName NVARCHAR(20) NOT NULL,
+    ActionText NVARCHAR(20) NOT NULL,
+    StateSetTo NVARCHAR(20) NOT NULL,
+    DeviceTypeID INT NOT NULL,
+    FunctionID INT NULL,
+    FOREIGN KEY (DeviceTypeID) REFERENCES [dbo].[DeviceTypes](DeviceTypeID),
+    FOREIGN KEY (FunctionID) REFERENCES [dbo].[Functions](FunctionID)
+);
+
+
+-- [dbo].[Locations] location of the devices.
 CREATE TABLE [dbo].[Locations] (
     LocationID INT NOT NULL IDENTITY(100000,1) PRIMARY KEY,
     Name NVARCHAR(100) NOT NULL,
@@ -49,6 +86,15 @@ CREATE TABLE [dbo].[Locations] (
     FOREIGN KEY (CreatedByUserID) REFERENCES [dbo].[Users](UserID)
 );
 
+-- [dbo].[DeviceStates] The sates a given device can be, These are "PENDING", "REGISTERD",
+-- "ACTIVE", "DEACTIVATED", "REJECTED", .etc.
+CREATE TABLE [dbo].[DeviceStates] (
+    DeviceStateID INT NOT NULL IDENTITY(100000,1) PRIMARY KEY,
+    StateName NVARCHAR(20) NOT NULL,
+    StateDescription NVARCHAR(MAX) NOT NULL,
+)
+
+-- [dbo].[Devices] List of devices
 CREATE TABLE [dbo].[Devices] (
     DeviceID INT NOT NULL IDENTITY(100000,1) PRIMARY KEY,
     SerialNumber VARCHAR(20) NOT NULL,
@@ -64,9 +110,11 @@ CREATE TABLE [dbo].[Devices] (
     MacAddress CHAR(17) NULL,
     FOREIGN KEY (CreatedUserID) REFERENCES [dbo].[Users](UserID),
     FOREIGN KEY (LocationID) REFERENCES [dbo].[Locations](LocationID),
-    FOREIGN KEY (DeviceTypeID) REFERENCES [dbo].[DeviceTypes](DeviceTypeID)
+    FOREIGN KEY (DeviceTypeID) REFERENCES [dbo].[DeviceTypes](DeviceTypeID),
+    FOREIGN KEY (DeviceStatus) REFERENCES [dbo].[DeviceStates](DeviceStateID)
 );
 
+-- [dbo].[Events] List of all events. 
 CREATE TABLE [dbo].[Events] (
     EventID INT NOT NULL IDENTITY(100000,1) PRIMARY KEY,
     EventValueID INT NOT NULL,
@@ -105,6 +153,7 @@ LEFT JOIN [dbo].[Locations] l ON d.LocationID = l.LocationID
 
 SELECT * FROM [dbo].[vw_AllEvents] ORDER BY EventDateTime DESC;
 
+-- view with last occured events for all devices.
 CREATE VIEW [dbo].[vw_LatestEvents] AS
 WITH LatestEvents AS (
     SELECT 
@@ -139,3 +188,6 @@ LEFT JOIN [dbo].[Devices] d ON e.DeviceID = d.DeviceID
 LEFT JOIN [dbo].[DeviceTypes] dt ON d.DeviceTypeID = dt.DeviceTypeID
 LEFT JOIN [dbo].[Locations] l ON d.LocationID = l.LocationID
 WHERE le.RowNum = 1;
+
+
+-- A view that has all the device, where the staus is active. 
